63e11bbb0534ee58810c815a9eaffbe76843fde1
Alan Pason <alan.pason132@gmail.com>
2023-10-21 20:19:28 +0200

    Connect: MongoDB

--- server/index.js
+++ server/index.js
@@ -1,5 +1,14 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const path = requrie("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const PORT = process.env.PORT || 5001;
 
@@ -9,6 +18,41 @@ app.use(cors());
 
 app.get("/api", (req, res) => {
   res.json({message: "Hello from server!"});
+});
+
+// Set up mongoose connection
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+const mongoDB = process.env.DB_URL;
+
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+}
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(session({secret: process.env.SESSION_SECRET, resave: false, saveUninitialized: true}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.urlencoded({extended: false}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
 });
 
 app.listen(PORT, () => {
